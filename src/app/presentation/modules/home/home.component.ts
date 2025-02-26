5380536cb13c0ced735f7822161fee82668435fb
MisaelGG11 <magomez@innovacion.gob.sv>
2025-02-26 16:13:26 -0600

    feat: add new validation handler for angular form with joi

--- src/app/presentation/modules/home/home.component.ts
+++ src/app/presentation/modules/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { toast } from 'ngx-sonner';
 
 import { ButtonModule } from 'primeng/button';
 import { ColorPickerModule } from 'primeng/colorpicker';
 import { DatePickerModule } from 'primeng/datepicker';
+import { CommonModule } from '@angular/common';
+import { createValidatorFromSchema } from '@app/validators/joi-validator.validator';
+import Joi from 'joi';
+import { CustomInputComponent } from '@app/presentation/components/inputs/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule, ColorPickerModule, FormsModule, DatePickerModule],
+  imports: [ButtonModule, ColorPickerModule, FormsModule, DatePickerModule, ReactiveFormsModule, CommonModule, CustomInputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,6 +21,38 @@ export default class HomeComponent {
   date: any
 
   click(){
-    toast('Hello World')
+    toast.success('Hello World', {
+      description: 'This is a description'
+    })
+  }
+
+  userForm: FormGroup;
+  userFormSchema = Joi.object({
+    username: Joi.string().required(),
+    email: Joi.string().email({ tlds: { allow: false }}).required(),
+    age: Joi.number().min(18).max(99).required()
+  });
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      username: [''],
+      email: [''],
+      age: [''],
+    },
+    {
+      validator: createValidatorFromSchema(this.userFormSchema)
+    }
+    );
+
+  }
+
+  submit() {
+    this.userForm.markAllAsTouched();
+    if (this.userForm.valid) {
+      console.log('Formulario válido:', this.userForm.value);
+    } else {
+      console.log('Formulario inválido');
+      console.log(this.userForm);
+    }
   }
 }
---
+++ src/app/validators/joi-validator.validator.ts
@@ -0,0 +1,50 @@
+
+import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
+import Joi, { ObjectSchema } from 'joi'
+import * as spanishMessages from './translation/es_ES.json';
+
+export function createValidatorFromSchema(schema: ObjectSchema) {
+  const validator = (group: FormGroup): ValidationErrors | null => {
+    // This is where the validation on the values of
+    // the form group is run.
+    const result = schema.validate(group.value, {
+      abortEarly: false,
+      messages:  spanishMessages
+      }
+    );
+
+    console.log(result.error?.details);
+
+    if (result.error) {
+      // Create an object to hold the errors
+      const errorObj: ValidationErrors = {};
+
+      // Loop through the Joi error details and
+      // create an object that Angular can use
+      // to display the errors in the form.
+      result.error.details.forEach((error) => {
+        // Get the field name
+        const path = error.path as string[];
+        const field = path.join('.');
+
+        // Set the error message
+        errorObj[field] = error.message;
+      });
+
+      for (const key in errorObj) {
+        const control = group.get(key);
+        if (control) {
+          control.setErrors({ joiError: errorObj[key] });
+        }
+      }
+
+      // Return the error object so that we can access
+      // the form’s errors via `form.errors`./*  */
+      return errorObj;
+    } else {
+      return null;
+    }
+  };
+
+  return validator;
+}
