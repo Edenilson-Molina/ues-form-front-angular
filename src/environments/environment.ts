3e1a1f3dec90858eb9268c1f91b17fc2b8d5a12d
David DV <71719478+daiv05@users.noreply.github.com>
2025-05-25 17:55:07 -0600

    feat: test prod server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiPublic: "http://10.175.160.11:8321/public",
-  api: "http://10.175.160.11:8321/api",
+  apiPublic: "https://ues-forms.wood-agent.site/public",
+  api: "http://ues-forms.wood-agent.site/api",
   secret: 'is-this-safe-naa',
 };
